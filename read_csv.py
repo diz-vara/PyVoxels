df54675a594ab01857d2315691cfdfd3cfe57fc7
diz_vara <avarfolomeev@navmii.com>
2018-03-23 17:36:04 +0300

    read_full_cloud

--- read_csv.py
+++ read_csv.py
@@ -9,6 +9,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D #<-- Note the capitalization! 
+import cv2
 
 def read_cloud_csv(cloud_num, base_dir = 'E:\\Data\\Voxels\\London-cal1\\selected\\'):
 
@@ -20,12 +21,30 @@ def read_cloud_csv(cloud_num, base_dir = 'E:\\Data\\Voxels\\London-cal1\\selecte
             pnt = np.array([float(p) for p in row])
             points.append(pnt)
         
-    cloud = np.array(points)        
+    cloud = np.array(points)     
+    cloud[:,1] = cloud[:,1] * -1
     return cloud
     
+
+
+def read_full_cloud_csv(cloud_num, base_dir = 'E:\\Data\\Voxels\\London-cal1\\London-cal1_drive_0015_sync\\velodyne_points\\csv'):
+
+    fname = base_dir + '\\{:010}.csv'.format(cloud_num)
+    with open (fname) as csvfile:
+        pclreader = csv.reader(csvfile, delimiter = ' ')
+        points = []
+        for row in pclreader:
+            pnt = np.array([float(p) for p in row])
+            points.append(pnt)
+        
+    cloud = np.array(points)     
+    cloud = cloud[:,[1,3,2]]
+    cloud[:,1] = cloud[:,1] * -1
+    return cloud
+
     
         
-def scatt3d(ax, cloud, clear = False, color = 'g', marker = 'o', size=3):
+def scatt3d(ax, cloud, clear = False, color = 'g', marker = 'o', size=25):
     cloud = np.array(cloud)
     if (clear):
         ax.cla()    
@@ -34,10 +53,10 @@ def scatt3d(ax, cloud, clear = False, color = 'g', marker = 'o', size=3):
     ax.scatter3D(cloud[:,0], cloud[:,1], cloud[:,2], 
                  c = color, 
                  marker=marker,
-                 s=size)
+                 s=size,edgecolors='face')
 
 
-def scatt2d(ax, cloud, clear = False, color = 'g', marker = 'o', size=3):
+def scatt2d(ax, cloud, clear = False, color = 'g', marker = 'o', size=25):
     cloud = np.array(cloud)
     if (clear):
         ax.cla()    
@@ -46,5 +65,12 @@ def scatt2d(ax, cloud, clear = False, color = 'g', marker = 'o', size=3):
     ax.scatter(cloud[:,0], cloud[:,1], 
                  c = color, 
                  marker=marker,
-                 s=size)
+                 s=size,edgecolors='face')
 
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+    return img
+    
