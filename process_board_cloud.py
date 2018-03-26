0197e3761b9ef22364e7f2be544eb438ce5bbc04
diz_vara <avarfolomeev@navmii.com>
2018-03-26 14:59:13 +0300

    cloud grid re-sorting

--- process_board_cloud.py
+++ process_board_cloud.py
@@ -121,7 +121,8 @@ def get_cloud_rotation(cloud):
     v1 = p3-p1
     nrm = np.cross(v0,v1)
     nrm = nrm/np.linalg.norm(nrm)
-
+    #print(v0,v1,nrm)
+    
     target = np.array([1,0,0]); 
     rot = find_rotation(nrm, target);
     return avg, rot    
@@ -168,14 +169,32 @@ def top_left(box):
         return p0
         
 #%%
+# re-sorts grid in order top corner->right corner
+def sort_grid(rotated_grid):
+    #re-arrange rotated_grid 
+    top_idx = np.argmax(rotated_grid[:,2])
+    right_idx= np.argmax(rotated_grid[:,1])
+    left_idx = np.argmin(rotated_grid[:,1])
+    
+    #print(top_idx, right_idx, left_idx)
+    
+    sign = np.sign(right_idx-top_idx)
+    col_idx = np.arange(top_idx, right_idx+sign, int((right_idx-top_idx+sign)/10))
+
+    sign = np.sign(left_idx-top_idx)
+    row_idx = np.arange(0, (left_idx-top_idx+sign), int((left_idx-top_idx+sign)/10))
+    
+    idx = np.array([row + col for row in row_idx for col in col_idx])
+    
+    return (rotated_grid[idx])
+    
+#%%
 def calc_cloud_grid(num,ax=None):
     cloud = read_cloud_csv(num)
-    #new_axes = np.array([1,2,0]);
-    n_cloud = cloud #[:,new_axes]
     
-    avg, rot = get_cloud_rotation(n_cloud)
+    avg, rot = get_cloud_rotation(cloud)
     
-    flat = rotate_cloud(n_cloud,avg,rot)
+    flat = rotate_cloud(cloud,avg,rot)
     
     box = get_box(flat)
     
@@ -190,38 +209,31 @@ def calc_cloud_grid(num,ax=None):
     #second rotation - to the original box image
     rotated_grid = np.array(rotated_grid*la.pinv(rot).transpose() + avg)
     
+    sorted_grid = sort_grid(rotated_grid)
 
-    #old_axes = np.array([2,0,1])
+
     
-    #o_cloud = cloud[:,old_axes]
-    o_rotated_grid = rotated_grid #[:,old_axes]    
-
     if (ax):
-        scatt3d(ax,cloud,True,'b')
-        scatt3d(ax,o_rotated_grid,False,'r')
+        scatt3d(ax,cloud,True,'#1f1f1f')
+        scatt3d(ax,sorted_grid,False,None,'d',49)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
-        ax.invert_yaxis();
+        left_border = np.ceil( max(cloud[:,1]) * 5.) / 5.
+        right_border = np.floor( min(cloud[:,1]) * 5.) / 5.
+        ax.set_ylim(left_border, right_border)
         #ax_3d.set_zlim(-1,1)
         #ax.axis('equal')
 
-    return cloud, o_rotated_grid
+    return cloud, sorted_grid
      
 #%%
 num=570
-c,g = calc_cloud_grid(704)
-imgname = 'E:\\Data\\Voxels\\London-cal1\\selected_raw\\{:010d}.png'.format(num)
+c,g = calc_cloud_grid(num)
 
 
-img = cv2.imread(imgname,-1)
-grey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,corners=cv2.findChessboardCorners(grey,(11,11))
-corners = corners[:,0,:]
+corners = find_image_corners(num)[:,0,:]
 
-#120- ... for 704!!!
-idx = np.array([row + i for i in range(11) for row in np.arange(0,121,11)])
-corn2 = corners[idx,:]
 
 ret, rot, t = cv2.solvePnP(g,corn2,mtx,dist,flags=cv2.SOLVEPNP_ITERATIVE)
 imgpts, jac = cv2.projectPoints(g, rot, t, mtx, dist)
