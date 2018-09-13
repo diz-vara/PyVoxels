ada40ddf04fe1cf473c5ecd155c3c70a8e6bef61
diz_vara <avarfolomeev@navmii.com>
2018-09-13 15:27:51 +0300

    new logs (ggaex etc)

--- pwrpak_reader.py
+++ pwrpak_reader.py
@@ -37,7 +37,63 @@ def week_seconds_to_utc(gpsweek,gpsseconds,leapseconds):
     elapsed = datetime.timedelta(days=(gpsweek*7),seconds=(gpsseconds+leapseconds))
     return (epoch+elapsed).timestamp()
 
-def read_pwrpak_log(nmea_dir):
+    
+    
+#%%
+def read_ggaex_log(ggax_file):
+    
+    point0 = {'time':0., 'lat':0., 'lon':0., 'alt_a':0., 'alt_g':0., 'quality':0.,
+             'bl_north':0., 'bl_east':0., 'bl_up':0, 'bl_length':0., 'bl_course':0., 'bl_pitch':0.,
+             'year':0, 'month':0, 'day':0,
+             'rmc_course':-1., 'true_course':-1.,
+             'imu_roll':0., 'imu_pitch':0., 'imu_yaw':0.}
+             
+    imu0 = {'time':0, 'accX':0, 'accY':0, 'accZ':0,
+               'gyrX':0, 'gyrY':0, 'gyrZ':0}
+
+    imu = []           
+    points = []
+    cnt = 0
+
+    with open (ggax_file) as csvfile:
+        pclreader = csv.reader(csvfile, delimiter = ',', skipinitialspace=True)
+        for row in pclreader:
+            if ("$GPGGAEX" in row):
+                point = point0.copy()
+                time = float(row[1])
+                point['time'] = time
+                lat = dm2degrees(float(row[2]))
+                if (row[3] != 'N'):
+                    lat = -lat;
+                point['lat'] = lat   
+                lon = dm2degrees(float(row[4]))
+                if (row[5] != 'E'):
+                    lon = -lon    
+                point['lon'] = lon    
+                point['alt_a'] = float(row[9])
+                point['alt_g'] = float(row[11])
+                point['quality'] = int(row[6])
+
+                roll_str = row[20].split('*')[0];
+                point['imu_roll'] = float( roll_str )
+                point['imu_pitch'] = float(row[19])
+                point['imu_yaw'] = float(row[18])
+                points.append(point)
+
+                
+                _imu = imu0.copy()
+                _imu['time'] = time
+                
+                _imu['accZ'] = float(row[15])
+                _imu['accY'] = float(row[16])
+                _imu['accX'] = float(row[17])
+                imu.append(_imu)
+                cnt += 1
+
+    return points, imu
+               
+#%%
+def read_pwrpak_log(fname):
     
     point0 = {'time':0., 'lat':0., 'lon':0., 'alt_a':0., 'alt_g':0., 'quality':0.,
              'bl_north':0., 'bl_east':0., 'bl_up':0, 'bl_length':0., 'bl_course':0., 'bl_pitch':0.,
@@ -51,18 +107,10 @@ def read_pwrpak_log(nmea_dir):
     points = []
     point = point0.copy();
     cnt = np.zeros(20)
-    
-    fname = nmea_dir + '/NMEA_sentence.txt'
-    tsname = nmea_dir + '/timestamps.txt'
 
-    tsfile = open(tsname)
     with open (fname) as csvfile:
         pclreader = csv.reader(csvfile, delimiter = ',', skipinitialspace=True)
         for row in pclreader:
-            tstr = tsfile.readline().split('.')
-            ts_s = int(tstr[0])
-            ts_ns = int(tstr[1])
-            ros_ts = ROS_ts(ts_s, ts_ns)
             if ( '$--GPGGA' in row):
                 #process GGA sentence
                 time = hms2seconds(float(row[1]))
@@ -207,6 +255,24 @@ def read_pwrpak_log(nmea_dir):
                 
                 
                 cnt[1] += 1
+            elif ('#CORRIMUDATAA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])#.split(';')[0])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (row[0] + " " + str(ts))
+                _imu = imu0.copy()
+                _imu['time'] = ts
+                
+                _imu['accY'] = float(row[15])
+                _imu['accX'] = float(row[14])
+                _imu['accZ'] = float(row[16].split('*')[0])
+                imu.append(_imu)
+                
+                
+                
+                cnt[1] += 1
             elif ('#TIMEA' in row):
                 point = point0.copy()
                 # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
@@ -280,10 +346,267 @@ def read_pwrpak_log(nmea_dir):
                 point['imu_yaw'] = float(row[12])
                 points.append(point)
                 cnt[9] += 1
+                
             else:
                 print ('[[' + row[0] + ']]')
                 #row = next(pclreader)
-                cnt[10] += 1
+                cnt[19] += 1
+
+                
+                
+        
+    return np.array(points), np.array(imu), cnt
+
+#%%
+    
+def read_pwrpak_dir(nmea_dir):
+    
+    point0 = {'time':0., 'lat':0., 'lon':0., 'alt_a':0., 'alt_g':0., 'quality':0.,
+             'bl_north':0., 'bl_east':0., 'bl_up':0, 'bl_length':0., 'bl_course':0., 'bl_pitch':0.,
+             'year':0, 'month':0, 'day':0,
+             'rmc_course':-1., 'true_course':-1.,
+             'imu_roll':0., 'imu_pitch':0., 'imu_yaw':0.}
+             
+    imu0 = {'time':0, 'accX':0, 'accY':0, 'accZ':0,
+               'gyrX':0, 'gyrY':0, 'gyrZ':0}
+    imu = []           
+    points = []
+    point = point0.copy();
+    cnt = np.zeros(20)
+    
+    fname = nmea_dir + '/NMEA_sentence.txt'
+    tsname = nmea_dir + '/timestamps.txt'
+
+    tsfile = open(tsname)
+    with open (fname) as csvfile:
+        pclreader = csv.reader(csvfile, delimiter = ',', skipinitialspace=True)
+        for row in pclreader:
+            tstr = tsfile.readline().split('.')
+            ts_s = int(tstr[0])
+            ts_ns = int(tstr[1])
+            ros_ts = ROS_ts(ts_s, ts_ns)
+            if ( '$--GPGGA' in row):
+                #process GGA sentence
+                time = hms2seconds(float(row[1]))
+                if (time != point['time']):
+                    if (point['quality'] != 0):
+                        points.append(point)
+                    point = point0.copy();
+                    point['time'] = time;
+                
+                lat = dm2degrees(float(row[2]))
+                if (row[3] != 'N'):
+                    lat = -lat;
+                point['lat'] = lat   
+                lon = dm2degrees(float(row[4]))
+                if (row[5] != 'E'):
+                    lon = -lon    
+                point['lon'] = lon    
+                point['alt_a'] = float(row[9])
+                point['alt_g'] = float(row[11])
+                point['quality'] = int(row[6])
+                print (row[0], row[1])
+            elif  ('$GPRMC' in row):
+                time = hms2seconds(float(row[1]))
+                if (time != point['time']):
+                    if (point['quality'] != 0):
+                        points.append(point)
+                    point = point0.copy();
+                    point['time'] = time;
+                point['rmc_course'] = float(row[8])
+                print (row[0], row[1])
+                
+            elif  ('$GPHDT' in row):
+                #process GPHDT 
+                if (row[1]):
+                    point['true_course'] = float(row[1])
+            elif  ('$GPVTG' in row):
+                #process GPVTG
+                print (row[0], row[1])
+            elif  ('$GPZDA' in row):
+                #process GPZDA
+                time = hms2seconds(float(row[1]))
+                if (time != point['time']):
+                    if (point['quality'] != 0):
+                        points.append(point)
+                    point = point0.copy();
+                    point['time'] = time;
+                point['day'] = int(row[2])
+                point['month'] = int (row[3])
+                point['year'] = int (row[4])
+                print (row[0], row[1])
+            elif  ('$PNVGIMU' in row):
+                #process PNVGIMU
+                time = hms2seconds(float(row[1]))
+                if (time != point['time']):
+                    if (point['quality'] != 0):
+                        points.append(point)
+                    point = point0.copy();
+                    point['time'] = time;
+
+                point['imu_roll'] = float(row[2])
+                point['imu_pitch'] = float(row[3])
+                point['imu_yaw'] = float(row[4])                
+                print (row[0], row[1])
+            elif  ('$PNVGBLS' in row):
+                #process PNVGBLS
+
+                time = hms2seconds(float(row[1]))
+                if (time != point['time']):
+                    if (point['quality'] != 0):
+                        points.append(point)
+                    point = point0.copy();
+                    point['time'] = time;
+
+                if (row[2]):
+                    point['bl_north'] = float(row[2])
+                if (row[3]):
+                    point['bl_east'] = float(row[3])
+                if (row[4]):
+                    point['bl_up'] = float(row[4])
+                if (row[5]):
+                    point['bl_length'] = float(row[5])
+                if (row[6]):
+                    point['bl_course'] = float(row[6])
+                if (row[7]):
+                    point['bl_pitch'] = float(row[7])
+                print (row[0], row[1])
+            elif ('--<INSPVA' in row):
+                point = point0.copy()
+                #first string is a header, skip it
+                row = next(pclreader)
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[1])
+                seconds = float(row[2])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                print (ts)
+                # LEAP SECONDS !!!!
+                point['time'] = ts
+                point['lat'] = float(row[3])
+                point['lon'] = float(row[4])
+                point['alt_a'] = float(row[5])
+                point['imu_roll'] = float(row[9])
+                point['imu_pitch'] = float(row[10])
+                point['imu_yaw'] = float(row[11])
+                points.append(point)
+            elif ('--#INSPVAXA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                # LEAP SECONDS !!!!
+                point['time'] = ts
+                point['lat'] = float(row[11])
+                point['lon'] = float(row[12])
+                point['alt_a'] = float(row[13])
+                point['alt_t'] = float(row[14])
+
+                point['imu_roll'] = float(row[18])
+                point['imu_pitch'] = float(row[19])
+                point['imu_yaw'] = float(row[20])
+                points.append(point)
+                cnt[0] += 1
+            elif ('%RAWIMUSXA' in row or '[COM1]%RAWIMUSXA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[4])
+                seconds = float(row[5])#.split(';')[0])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (row[0] + " " + str(ts))
+                _imu = imu0.copy()
+                _imu['time'] = ts
+                
+                _imu['accZ'] = int(row[7])
+                _imu['accY'] = -int(row[8])
+                _imu['accX'] = int(row[9])
+                _imu['gyrZ'] = int(row[10])
+                _imu['gyrY'] = -int(row[11])
+                _imu['gyrX'] = int (row[12].split('*')[0])
+                imu.append(_imu)
+                
+                
+                
+                cnt[1] += 1
+            elif ('#TIMEA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                cnt[2] += 1
+            elif ('#BESTGNSSPOSA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                cnt[3] += 1
+
+            elif ('#BESTPOSA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                cnt[4] += 1
+            elif ('#HEADING2A' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                cnt[5] += 1
+            elif ('#RANGECMPA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                cnt[6] += 1
+                
+            elif ('#RAWEPHEMA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                week = int(row[5])
+                seconds = float(row[6])
+                ts = week_seconds_to_utc(week,seconds,0)                
+                #print (ts)
+                cnt[7] += 1
+            elif ('#RXCONFIGA' in row):
+                week = 0                
+                cnt[8] += 1
+            elif ('%INSPVASA' in row):
+                point = point0.copy()
+                # week seconds lat lon alt Nvel Evel Uvel Roll Pith Azim 
+                # I use week from the short header
+                week = int(row[1])
+                #but seconds from the message body
+                seconds = float(row[3])
+                ts = week_seconds_to_utc(week,seconds,0)     
+                print (row[3],seconds, ts)
+                # LEAP SECONDS !!!!
+                point['time'] = ros_ts.double();
+                point['lat'] = float(row[4])
+                point['lon'] = float(row[5])
+                point['alt_a'] = float(row[6])
+
+                point['imu_roll'] = float(row[10])
+                point['imu_pitch'] = float(row[11])
+                point['imu_yaw'] = float(row[12])
+                points.append(point)
+                cnt[9] += 1
+                
+            else:
+                print ('[[' + row[0] + ']]')
+                #row = next(pclreader)
+                cnt[19] += 1
 
                 
                 
@@ -354,14 +677,14 @@ def nvs2lla_course(nvs):
 
 
 
-def plot_courses(ax, enu_course, num, bImu = False, bBl = False, len = 100):
+def plot_courses(ax, enu_course, num, bImu = False, bBl = False, len = 100, corr=0):
     x0 = enu_course[num][0]
     y0 = enu_course[num][1]
 
     #
     #rmc_course=np.radians(enu_course[num][5]) * -1
-    bl_course=np.radians(enu_course[num][4]-90)  * -1
-    imu_course=np.radians(enu_course[num][8]) * -1
+    bl_course=np.radians(enu_course[num+corr][4]-90)  * -1
+    imu_course=np.radians(enu_course[num+corr][8]) * -1
 
     x1_ = 0
     y1_ = len
