7e4a2f7556e3e1846baa11c73ab0460d86b021ac
diz_vara <avarfolomeev@navmii.com>
2018-11-22 09:19:02 +0300

    getTimesAcc correction

--- getTimesAcc.py
+++ getTimesAcc.py
@@ -4,13 +4,15 @@ Created on Fri Oct 26 12:06:52 2018
 
 @author: avarfolomeev
 """
+import numpy as np
+from pwrpak_reader import *
 
 
-def get_times_acc(pwkpak, imu):
+def get_times_acc(pwrpak, imu):
     p_t_ = np.array([i['time'] for i in pwrpak])
     accY_ = np.array([i['accY'] for i in pwrpak])
-    o_t = np.array([o['time'] for o in o_imu])
-    o_x = np.array([o['accX'] for o in o_imu])
+    o_t = np.array([o['time'] for o in imu])
+    o_x = np.array([o['accX'] for o in imu])
 
     return {"pwrTime":p_t_, 
             "ousterTime":o_t,
