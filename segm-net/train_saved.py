cf1471cd86249e4e9210ae45902db9d45ac079ce
diz@ub14 <dizvara@gmail.com>
2019-03-05 09:06:35 +0300

    another attempt

--- segm-net/train_saved.py
+++ segm-net/train_saved.py
@@ -148,7 +148,7 @@ sess = tf.Session(config = config)
 
 #saver = tf.train.Saver()
 
-load_net = '/media/avarfolomeev/storage/Data/Segmentation/vox_segm/vox-net-3-8003'  #lp-7004'
+load_net = '/media/avarfolomeev/storage/Data/Segmentation/vox_segm/vox-net-3-8703'  #lp-7004'
 
 min_loss_name = 'min_loss.txt'
 
@@ -206,7 +206,7 @@ writer = tf.summary.FileWriter('/media/avarfolomeev/storage/Data/Segmentation/lo
 
 print('training')
 train_nn(sess, epochs, batch_size, get_train_batches_fn, get_val_batches_fn, train_op,
-         loss, input_image, correct_label, keep_prob, learning_rate, 8010) 
+         loss, input_image, correct_label, keep_prob, learning_rate, 8800) 
 
 
 
