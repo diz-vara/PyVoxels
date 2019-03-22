1441974877ee4b23344d3351d0c83e96bb4db369
Anton (home) <diz.vara@gmail.com>
2019-03-22 22:56:22 +0300

    another (working) version of read_frozen

--- voxSigns/read_frozen.py
+++ voxSigns/read_frozen.py
@@ -10,14 +10,17 @@ import tensorflow as tf
 
 
 
-def read_frozen(frozen_path):
-    new_graph = tf.Graph()
-    
-    with new_graph.as_default():
+def read_frozen(frozen_graph_filename):
+    # We load the protobuf file from the disk and parse it to retrieve the 
+    # unserialized graph_def
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
         graph_def = tf.GraphDef()
-        with tf.gfile.GFile(frozen_path, 'rb') as fid:
-            graph_def.ParseFromString(fid.read())
-            tf.import_graph_def(graph_def, name = '')
-    
+        graph_def.ParseFromString(f.read())
 
-    return new_graph
+    # Then, we import the graph_def into a new Graph and returns it 
+    with tf.Graph().as_default() as graph:
+        # The name var will prefix every op/nodes in your graph
+        # Since we load everything in a new graph, this is not needed
+        tf.import_graph_def(graph_def, name="")
+    return graph
+    
